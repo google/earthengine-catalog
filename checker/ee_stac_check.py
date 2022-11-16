cf07c06e43269128bf431f6dffc79bcebb631862
Kurt Schwehr <schwehr@google.com>
2022-11-16 11:50:50 -0800

    ee_stac_check: Print an instruction message when errors are encountered
    PiperOrigin-RevId: 488998942

--- checker/ee_stac_check.py
+++ checker/ee_stac_check.py
@@ -17,12 +17,41 @@ from checker import tree
 _CHECKS = flags.DEFINE_multi_string(
     'checks', [], 'List of checks to run or empty to run all checks.')
 
+INSTRUCTIONS_FOR_HANDLING_ERRORS = """
+ee_stac_check has encountered some issues.  Here are some suggestions for how to
+handle the issues.
+
+* If you get stuck, please ask us for help. It's okay to create a pull
+  request (PR) with checker errors to start the conversation. We can work with
+  you to get a PR to a finished state.
+
+* Start from the top down.  The system is designed to guide people through the
+  fixing process in that order.
+
+* The issue messages may be difficult to understand. The checker is new and
+  the messages are under active development. Please ask questions and we will
+  work with you. We plan to open the issues feature on GitHub, but are not yet
+  ready to do so.  For now, ask here:
+
+  https://developers.google.com/earth-engine/help#dataset_problem
+
+* Some issues are purely because a value was outside of what has been seen with
+  the catalog before.  It is okay to change the range of a check or add a new
+  value to a list of accepted values.  We may ask you to explain the change
+  so we understand it.  A big part of the checker is capturing knowledge for
+  the entire community.
+
+* We welcome comments and suggestions on the checker.
+"""
+
 
 def find_issues(checks: list[str]) -> Iterator[stac.Issue]:
+  """Runs all of the checkers and yields any issues found."""
   stac_root = stac.stac_root()
   nodes = stac.load(stac_root)
 
   print('Number of STAC nodes loaded:', len(nodes))
+  print()
 
   for a_node in nodes:
     yield from node.run_checks(a_node, checks)
@@ -49,7 +78,13 @@ def main(argv: Sequence[str]) -> None:
     print('Warning count:', warning_count)
 
   if error_count:
+    print()
+    print('===============')
     print('Error count:', error_count)
+    print('===============')
+
+    print(INSTRUCTIONS_FOR_HANDLING_ERRORS)
+
     sys.exit(1)
 
 
