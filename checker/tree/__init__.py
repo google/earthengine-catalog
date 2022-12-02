f9baf83cba232a9ee030354d94a1b500c3864f55
Simon Ilyushchenko <simonf@google.com>
2022-12-01 19:28:39 -0800

    Remove the check banning single-use keywords.
    PiperOrigin-RevId: 492358298

--- checker/tree/__init__.py
+++ checker/tree/__init__.py
@@ -3,11 +3,9 @@
 from typing import Iterator
 
 from checker import stac
-from checker.tree import keywords
 from checker.tree import parent_child
 
 _CHECKS = [
-    keywords.Check,
     parent_child.Check,
 ]
 
