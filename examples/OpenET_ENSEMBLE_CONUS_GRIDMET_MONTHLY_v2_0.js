e0ddade76770e2a96f2efd4ae0e6304d64a92b7d
Kurt Schwehr <schwehr@google.com>
2023-01-04 09:14:29 -0800

    OpenET_ENSEMBLE_CONUS_GRIDMET_MONTHLY_v2_0.js: Update the image collection ID with the public data catalog ID

--- examples/OpenET_ENSEMBLE_CONUS_GRIDMET_MONTHLY_v2_0.js
+++ examples/OpenET_ENSEMBLE_CONUS_GRIDMET_MONTHLY_v2_0.js
@@ -1,5 +1,4 @@
-// TODO: Update the image collection ID with the public data catalog ID
-var dataset = ee.ImageCollection('projects/openet/ensemble/conus/gridmet/monthly/v2_0')
+var dataset = ee.ImageCollection('OpenET/ENSEMBLE/CONUS/GRIDMET/MONTHLY/v2_0')
   .filterDate('2020-01-01', '2021-01-01')
 
 var et = dataset.select(['et_ensemble_mad']).sum()
@@ -8,8 +7,8 @@ var visualization = {
   min: 0.0,
   max: 1400.0,
   palette: [
-    '9E6212', 'AC7D1D', 'BA9829', 'C8B434', 'D6CF40', 'BED44B', '9FCB51', '80C256', '61B95C', '42B062', 
-    '45B677', '49BC8D', '4DC2A2', '51C8B8', '55CECE', '4DB4BA', '459AA7', '3D8094', '356681', '2D4C6E', 
+    '9E6212', 'AC7D1D', 'BA9829', 'C8B434', 'D6CF40', 'BED44B', '9FCB51', '80C256', '61B95C', '42B062',
+    '45B677', '49BC8D', '4DC2A2', '51C8B8', '55CECE', '4DB4BA', '459AA7', '3D8094', '356681', '2D4C6E',
   ]
 };
 
