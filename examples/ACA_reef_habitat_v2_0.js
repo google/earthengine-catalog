f1fcbf3d32d8abdce8027cceeb9a3f91d8af5069
Kurt Schwehr <schwehr@google.com>
2023-02-02 10:56:54 -0800

    (Cleanup) Allen Coral Atlas (ACA) examples: Reorder bands to have the bands look more similar to the preview image
    Also:
    
    - Have version 1.0 match version 2.0
    - Convert double quotes to single to they are all the same
    - snake_case -> camelCase following guidance: https://google.github.io/styleguide/jsguide.html#naming
    - Added periods to comments following examples: https://google.github.io/styleguide/jsguide.html#formatting-block-comment-style
    Removed self-evident "map" from layer names
    
    PiperOrigin-RevId: 506668838

--- examples/ACA_reef_habitat_v2_0.js
+++ examples/ACA_reef_habitat_v2_0.js
@@ -1,19 +1,19 @@
-var dataset = ee.Image("ACA/reef_habitat/v2_0");
+var dataset = ee.Image('ACA/reef_habitat/v2_0');
 
-// Teti'aroa, an atoll in French Polynesia
+// Teti'aroa, an atoll in French Polynesia.
 Map.setCenter(-149.56194, -17.00872, 13);
 Map.setOptions('SATELLITE');
 
-// The visualisations are baked into the image properties
+// The visualisations are baked into the image properties.
 
-// Benthic habitat classification
-var benthic_habitat = dataset.select('benthic').selfMask();
-Map.addLayer(benthic_habitat, {}, "Benthic habitat map");
+// Example mask application.
+var reefExtent = dataset.select('reef_mask').selfMask();
+Map.addLayer(reefExtent, {}, 'Global reef extent');
 
-// Geomorphic zonation classification
-var geomorphic_zonation = dataset.select('geomorphic').selfMask();
-Map.addLayer(geomorphic_zonation, {}, "Geomorphic zonation map");
+// Geomorphic zonation classification.
+var geomorphicZonation = dataset.select('geomorphic').selfMask();
+Map.addLayer(geomorphicZonation, {}, 'Geomorphic zonation');
 
-// Example mask application
-var reef_extent = dataset.select('reef_mask').selfMask();
-Map.addLayer(reef_extent, {}, "Global reef extent");
+// Benthic habitat classification.
+var benthicHabitat = dataset.select('benthic').selfMask();
+Map.addLayer(benthicHabitat, {}, 'Benthic habitat');
