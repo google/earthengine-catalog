246f2dcd248d97777d3f205e61cc2c25b920bbac
Audra G <agriebel@contractor.usgs.gov>
2023-03-31 09:24:05 -0500

    Update USGS_NLCD_RELEASES_2019_REL_NALCMS.js
    updated colors

--- examples/USGS/USGS_NLCD_RELEASES_2019_REL_NALCMS.js
+++ examples/USGS/USGS_NLCD_RELEASES_2019_REL_NALCMS.js
@@ -4,25 +4,25 @@ print(image);
 
   Map.addLayer(image, {
   palette: [
-    'tomato',  // 1
-    'orange',  // 2
-    'red',  // 3
-    'purple',  // 4
-    'maroon',  // 5
-    'silver',  // 6
-    'blue',  // 7
-    'coral',  // 8
-    'cyan',  // 9
-    'bisque',  // 10
-    'darkviolet',  // 11
-    'goldenrod',  // 12
-    'lightsalmon',  // 13
-    'fuchsia',  // 14
-    'seagreen',  // 15
-    'whitesmoke',  // 16
-    'thistle',  // 17
-    'sandybrown',  // 18
-    'seashell',  // 19
+    '1a200e',  // 1
+    '4b5320',  // 2
+    '222b13',  // 3
+    '68ab5f',  // 4
+    'b5c58f',  // 5
+    '1c5f2c',  // 6
+    '556b2f',  // 7
+    'dec5c5',  // 8
+    'dcd939',  // 9
+    'ccb879',  // 10
+    'af963c',  // 11
+    'd1d182',  // 12
+    '82ba9e',  // 13
+    'a3cc51',  // 14
+    'ab6c28',  // 15
+    'b8d9eb',  // 16
+    'ab0000',  // 17
+    '466b9f',  // 18
+    'd1def8',  // 19
     ],
   min:1, max:19});
 
