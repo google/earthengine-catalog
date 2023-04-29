7fd867ca0698766eb1ef831720098a7c57e4396c
Kurt Schwehr <schwehr@google.com>
2023-04-28 17:40:53 -0700

    examples WRI/GFW/FORMA/alerts: Fix accidental paste
    PiperOrigin-RevId: 528028262

--- examples/WRI/WRI_GFW_FORMA_alerts.js
+++ examples/WRI/WRI_GFW_FORMA_alerts.js
@@ -1,4 +1,4 @@
-import {ACCOUNT_SPEND_DECIMAL_FORMAT} from 'google3/customer_support/cases/companion_apps/billing_assist/companion_app/shared/constants/constants';var dataset = ee.Image('WRI/GFW/FORMA/alerts');
+var dataset = ee.Image('WRI/GFW/FORMA/alerts');
 var formaAlerts = dataset.select('alert_delta');
 var formaAlertsVis = {
   min: 25,
