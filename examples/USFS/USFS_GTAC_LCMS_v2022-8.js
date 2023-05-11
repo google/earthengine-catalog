96906be0d43a9881776a5075291002f69ef17003
Simon Ilyushchenko <simonf@google.com>
2023-05-10 19:26:44 -0700

    Bring back filtering on year and study area in the LCMS example
    PiperOrigin-RevId: 531067493

--- examples/USFS/USFS_GTAC_LCMS_v2022-8.js
+++ examples/USFS/USFS_GTAC_LCMS_v2022-8.js
@@ -1,4 +1,11 @@
-var lcms = ee.ImageCollection('USFS/GTAC/LCMS/v2022-8').first();
+var lcms = ee.ImageCollection('USFS/GTAC/LCMS/v2022-8')
+
+var lcms = dataset
+    .filter(ee.Filter.and(
+      ee.Filter.eq('year', 2022),  // range: [1985, 2022]
+      ee.Filter.eq('study_area', 'CONUS')  // or 'SEAK'
+    ))
+    .first();
 
 Map.addLayer(lcms.select('Land_Cover'), {}, 'Land Cover');
 Map.addLayer(lcms.select('Land_Use'), {}, 'Land Use');
