a2c8197e91092d0e882737a1b3bad6a5119ad851
Kurt Schwehr <schwehr@google.com>
2023-05-16 10:14:34 -0700

    Introduce preview image scripts
    Previews:
    - ACA_reef_habitat_v2_0_preview.js - https://code.earthengine.google.com/decea897bc0b28e471aebf248eed07af
    - NOAA_GOES_16_FDCF_preview.js - https://code.earthengine.google.com/c83c5ce2bed20489849ecd6569ae5dec
    - OpenET_DISALEXI_CONUS_GRIDMET_MONTHLY_v2_0_preview.js - https://code.earthengine.google.com/b3715704c3d0b4311ae8f199a6cb38ef
    - OpenET_EEMETRIC_CONUS_GRIDMET_MONTHLY_v2_0_preview.js - https://code.earthengine.google.com/454d726d4ce59cf89b051d8c799340ba
    - OpenET_ENSEMBLE_CONUS_GRIDMET_MONTHLY_v2_0_preview.js - https://code.earthengine.google.com/f0bec1d5633386a9b351b4e6be67a902
    - USGS_NLCD_RELEASES_2019_REL_RCMAP_V5_COVER_preview.js - https://code.earthengine.google.com/bb96165b7a2b51db0adfb50d96094410
    PiperOrigin-RevId: 532497854

---
+++ examples/ACA/ACA_reef_habitat_v2_0_preview.js
@@ -0,0 +1,42 @@
+var dataset = ee.Image('ACA/reef_habitat/v2_0');
+
+// Teti'aroa, an atoll in French Polynesia.
+var lon = -149.56194;
+var lat = -17.00872;
+
+Map.setCenter(lon, lat, 13);
+Map.setOptions('SATELLITE');
+
+// Degrees in EPSG:3857
+var delta = 0.05;
+// Width and Height of the thumbnail image.
+var pixels = 256;
+
+ // cadetblue
+var background = ee.Image.rgb(95, 158, 160).visualize({min: 0, max: 255});
+
+// The visualisations are baked into the image properties.
+// Benthic habitat classification.
+var benthicHabitat = dataset.select('benthic').selfMask();
+
+var areaOfInterest = ee.Geometry.Rectangle(
+  [lon - delta, lat - delta, lon + delta, lat + delta], null, false);
+
+var visParams = {
+  dimensions: [pixels, pixels], region: areaOfInterest, crs: 'EPSG:3857',
+  format: 'png',
+};
+
+// TODO(b/267786257): Trouble here: This is just black and white.
+print(ui.Thumbnail({image: benthicHabitat, params: visParams}));
+
+var palette = [
+  'ffffbe', 'e0d05e', 'b19c3a', '668438',
+  'ff6161', '000000', '000000', '9bcc4f'];
+
+var image = benthicHabitat.visualize({palette: palette, min: 11, max:18});
+var imageWithBackground = ee.ImageCollection([background, image]).mosaic();
+
+Map.addLayer(imageWithBackground, null, 'Benthic habitat');
+
+print(ui.Thumbnail({image: imageWithBackground, params: visParams}));
