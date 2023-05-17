06743afa608fcf0aeb414dbbcdd07883af36aa22
Kurt Schwehr <schwehr@google.com>
2023-05-17 14:35:22 -0700

    checker/node/examples: Check image preview scripts
    All new datasets should have _preview.js scripts.
    
    Follow on to https://github.com/google/earthengine-catalog/pull/507
    
    PiperOrigin-RevId: 532911429

--- checker/stac.py
+++ checker/stac.py
@@ -58,6 +58,15 @@ def examples_root() -> pathlib.Path:
   return data_root() / 'examples/javascript_examples'
 
 
+def previews_root() -> pathlib.Path:
+  # First try for a local path for bazel.
+  path = pathlib.Path('examples')
+  if path.is_dir(): return path
+
+  # blaze has Fileset support
+  return data_root() / 'examples/javascript_previews'
+
+
 @dataclasses.dataclass
 class Node:
   """Container for one STAC Catalog or STAC Collection."""
