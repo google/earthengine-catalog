3b22b7d85dc79a893957369a0406649e1d909e4c
Kurt Schwehr <schwehr@google.com>
2023-05-24 13:42:38 -0700

    AAFC_ACI: cleanup
    - Fit to 80 columns
    - Remove unnecessary `.0`
    - Remove newlines from classes and visualization for readability
    - lookat: lon before lat to match `Map.setCenter()`
    - Use `ee.gee_terms_of_use(license)`
    - Add a layer name for `Map.addLayer()`
    
    PiperOrigin-RevId: 534966233

--- examples/AAFC/AAFC_ACI.js
+++ examples/AAFC/AAFC_ACI.js
@@ -3,4 +3,4 @@ var crop2016 = dataset
     .filter(ee.Filter.date('2016-01-01', '2016-12-31'))
     .first();
 Map.setCenter(-103.8881, 53.0371, 10);
-Map.addLayer(crop2016);
+Map.addLayer(crop2016, {}, '2016 Canada AAFC Annual Crop Inventory');
