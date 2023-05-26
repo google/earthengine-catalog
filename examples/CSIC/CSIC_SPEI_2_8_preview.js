ac1df1f747dc380b0dec7bf2fb455d43c010c409
B0RJA <borja.latorre@eead.csic.es>
2023-05-26 16:38:56 +0200

    Change revision suggestions

--- examples/CSIC/CSIC_SPEI_2_8_preview.js
+++ examples/CSIC/CSIC_SPEI_2_8_preview.js
@@ -1,13 +1,9 @@
-
-// Retrieve the last date from the SPEI dataset
-var dataset = ee.ImageCollection("CSIC/SPEI/2_8").
+// Retrieve the last date from the SPEI dataset.
+var dataset = ee.ImageCollection('CSIC/SPEI/2_8').
   filterDate('2021-12-01', '2022-01-01');
 
-// Convert ee.ImageCollection to ee.Image
-dataset = ee.Image(ee.List(dataset.toList(1)).get(0));
-
-// Select the 24-month analysis
-var spei24 = dataset.select('SPEI_24_month');
+// Select the 24-month analysis.
+var spei24 = dataset.select('SPEI_24_month').first();
 
 // Degrees in EPSG:3857.
 var delta = 60;
@@ -22,14 +18,14 @@ Map.setCenter(lon, lat, 2);
 var areaOfInterest = ee.Geometry.Rectangle(
   [-120, lat - delta, 60, lat + delta], null, false);
 
-// Set the visualization ranges and color palette
+// Set the visualization ranges and color palette.
 var vis = {
   min: -2.33,
   max:  2.33,
   palette: [
-    "#8B1A1A","#DE2929", "#F3641D",
-    "#FDC404", "#9AFA94", "#03F2FD",
-    "#12ADF3", "#1771DE", "#00008B"
+    '8B1A1A', 'DE2929', 'F3641D',
+    'FDC404', '9AFA94', '03F2FD',
+    '12ADF3', '1771DE', '00008B',
   ],
   region: areaOfInterest,
   dimensions: [pixels, pixels],
@@ -37,7 +33,7 @@ var vis = {
   format: 'png',
 };
 
-// Display the SPEI 24-month layer
+// Display the SPEI 24-month layer.
 Map.addLayer(spei24, vis, 'SPEI 24 month');
 
 print(ui.Thumbnail({image: spei24, params: vis}));
