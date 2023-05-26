d2a5f75bcae78919fb5c1966266bcfd28c970934
B0RJA <borja.latorre@eead.csic.es>
2023-05-26 16:33:40 +0200

    Change revision suggestions

--- examples/CSIC/CSIC_SPEI_2_8.js
+++ examples/CSIC/CSIC_SPEI_2_8.js
@@ -1,24 +1,23 @@
-
-// Retrieve the last date from the SPEI dataset
+// Retrieve the last date from the SPEI dataset.
 var dataset = ee.ImageCollection("CSIC/SPEI/2_8").
   filterDate('2021-12-01', '2022-01-01');
 
-// Select the 24-month analysis
+// Select the 24-month analysis.
 var spei24 = dataset.select('SPEI_24_month');
 
-// Set the visualization ranges and color palette
-var vis = {
+// Set the visualization ranges and color palette.
+var visParams = {
   min: -2.33,
   max:  2.33,
   palette: [
-    "#8B1A1A","#DE2929", "#F3641D",
-    "#FDC404", "#9AFA94", "#03F2FD",
-    "#12ADF3", "#1771DE", "#00008B"
+    '8B1A1A', 'DE2929', 'F3641D',
+    'FDC404', '9AFA94', '03F2FD',
+    '12ADF3', '1771DE', '00008B',
   ]
 };
 
-// Set the map center to Spain's location
-Map.setCenter(-3.74922, 40.463667, 4);
+// Set the map center to Spain's location.
+Map.setCenter(-3.75, 40.47, 4);
 
-// Display the SPEI 24-month layer
-Map.addLayer(spei24, vis, 'SPEI 24 month');
+// Display the SPEI 24-month layer.
+Map.addLayer(spei24, visParams, 'SPEI 24 month');
