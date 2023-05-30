16696201205d393b3fff650fae0ce709258aad3a
Kurt Schwehr <schwehr@google.com>
2023-05-30 09:16:05 -0700

    Switzerland/SWISSIMAGE/orthos/10cm: Add back the explicite `2017` so that it is an `ee.Image`
    https://code.earthengine.google.com/86464bd1b18d56a4949542233f49e852
    
    PiperOrigin-RevId: 536420293

--- examples/Switzerland/Switzerland_SWISSIMAGE_orthos_10cm.js
+++ examples/Switzerland/Switzerland_SWISSIMAGE_orthos_10cm.js
@@ -1,4 +1,4 @@
-var dataset = ee.Image('Switzerland/SWISSIMAGE/orthos/10cm');
+var dataset = ee.Image('Switzerland/SWISSIMAGE/orthos/10cm/2017');
 
 Map.setCenter(7.75, 46.02, 18);
 Map.addLayer(dataset, null, 'Switzerland 10 cm');
