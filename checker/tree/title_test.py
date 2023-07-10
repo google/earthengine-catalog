4165e5ecb55a63713f9df4b597eb1a0702de322e
Simon Ilyushchenko <simonf@google.com>
2023-07-10 00:11:17 -0700

    Fix title_test.py. Dataset titles are strings, not lists.
    PiperOrigin-RevId: 546779592

--- checker/tree/title_test.py
+++ checker/tree/title_test.py
@@ -18,9 +18,9 @@ FILE_PATH = pathlib.Path('test/path/should/be/ignored')
 class TitleTest(absltest.TestCase):
 
   def test_valid(self):
-    stac_data_a = {'title': ['Title A']}
+    stac_data_a = {'title': 'Title A'}
     collection_a = stac.Node(ID, FILE_PATH, COLLECTION, IMAGE, stac_data_a)
-    stac_data_b = {'title': ['Title B']}
+    stac_data_b = {'title': 'Title B'}
     collection_b = stac.Node(ID, FILE_PATH, COLLECTION, IMAGE, stac_data_b)
 
     nodes = [collection_a, collection_b]
@@ -28,15 +28,15 @@ class TitleTest(absltest.TestCase):
     self.assertEqual(0, len(issues))
 
   def test_catch_duplicate(self):
-    stac_data_a = {'title': ['Title A']}
+    stac_data_a = {'title': 'Title A'}
     collection_a = stac.Node(ID, FILE_PATH, COLLECTION, IMAGE, stac_data_a)
     collection_b = stac.Node(ID, FILE_PATH, COLLECTION, IMAGE, stac_data_a)
 
     nodes = [collection_a, collection_b]
     issues = list(Check.run(nodes))
     expect = [
-        Check.new_issue(collection_a, 'Non-unique title: "[\'Title A\']"'),
-        Check.new_issue(collection_b, 'Non-unique title: "[\'Title A\']"')]
+        Check.new_issue(collection_a, 'Non-unique title: "Title A"'),
+        Check.new_issue(collection_b, 'Non-unique title: "Title A"')]
     self.assertEqual(expect, issues)
 
 if __name__ == '__main__':
