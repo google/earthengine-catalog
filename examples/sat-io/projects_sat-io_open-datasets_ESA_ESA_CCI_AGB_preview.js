f5728112cf8dc1cfcbc0bc304a71ba9dd3f60d99
Samapriya Roy <samapriya.roy@gmail.com>
2023-10-01 10:49:09 -0500

    updated hex codes

--- examples/sat-io/projects_sat-io_open-datasets_ESA_ESA_CCI_AGB_preview.js
+++ examples/sat-io/projects_sat-io_open-datasets_ESA_ESA_CCI_AGB_preview.js
@@ -1,16 +1,16 @@
 var agb = ee.ImageCollection("projects/sat-io/open-datasets/ESA/ESA_CCI_AGB");
 
 var palette = [
-  "#C6ECAE",
-  "#A1D490",
-  "#7CB970",
-  "#57A751",
-  "#348E32",
-  "#267A29",
-  "#176520",
-  "#0C4E15",
-  "#07320D",
-  "#031807",
+  "C6ECAE",
+  "A1D490",
+  "7CB970",
+  "57A751",
+  "348E32",
+  "267A29",
+  "176520",
+  "0C4E15",
+  "07320D",
+  "031807",
 ];
 
 var snazzy = require("users/aazuspan/snazzy:styles");
