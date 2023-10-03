a23630c585d2d3413f5b3055dbca5f57da9c4fd5
Andrea Melchiorre <andrea.melchiorre@eodatascience.com>
2023-10-03 17:03:15 +0100

    added example and preview js files

---
+++ examples/geoscience-aus-cat/projects_geoscience-aus-cat_assets_ga_landcover_preview.js
@@ -0,0 +1,51 @@
+var landcover = ee.ImageCollection('projects/geoscience-aus-cat/assets/ga_landcover');
+
+var level3palette = [
+    '#ffffff', // No Data
+    '#acbc2d', // Cultivated terrestrial vegetation
+    '#0e7912', // Natural terrestrial vegetation
+    '#1ebf79', // Natural acquatic vegetation
+    '#da5c69', // Artificial surface
+    '#f3ab69', // Natural bare surface
+    '#4d9fdc'  // Water
+  ];
+
+var visualization = {
+  bands: ['remapped'],
+  min: 0,
+  max: 6,
+  palette: level3palette
+};
+
+var lon = 122.41280;
+var lat = -18.06402;
+
+Map.setCenter(lon, lat, 10);
+
+// Degrees in EPSG:3857.
+var delta = 0.25;
+// Width and height of the thumbnail image.
+var pixels = 256;
+
+var areaOfInterest = ee.Geometry.Rectangle(
+  [lon - delta, lat - delta, lon + delta, lat + delta], null, false);
+
+var image = landcover.filterDate('1995-01-01', '1996-01-01')
+                     .select('B04')
+                     .map(function(img){
+                       return img.remap(
+                        [0, 111, 112, 124, 215, 216, 220],
+                        [0, 1, 2, 3, 4, 5, 6]
+                       );
+                     }).mosaic();
+                  
+Map.addLayer(image, visualization, 'Land Cover');
+
+var imageParams = {
+  dimensions: [pixels, pixels],
+  region: areaOfInterest,
+  crs: 'EPSG:3857',
+  format: 'png',
+};
+
+print(ui.Thumbnail({image: image.visualize(visualization), params: imageParams}));
---
+++ examples/geoscience-aus-cat/projects_geoscience-aus-cat_assets_geomedians_LS8_preview.js
@@ -0,0 +1,36 @@
+var geomedian_ls5 = ee.ImageCollection('projects/geoscience-aus-cat/assets/geomedians_LS8');
+
+var geometry = 
+    /* color: #98ff00 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[112.48755322062398, -10.110164065391384],
+          [112.48755322062398, -39.68235603413486],
+          [154.147709470624, -39.68235603413486],
+          [154.147709470624, -10.110164065391384]]], null, false);
+          
+var composite = geomedian_ls5.filterBounds(geometry)
+                             .filterDate('2018-01-01', '2018-01-01')
+                             .mosaic();
+                             
+var visualization = {
+  bands: ['B03', 'B02', 'B01'],
+  min: 0,
+  max: 0.3
+};
+
+Map.centerObject(geometry);
+Map.addLayer(image, visualization, '2018 True Color Composite');
+
+var imageParams = {
+  dimensions: [pixels, pixels],
+  region: geometry,
+  crs: 'EPSG:3857',
+  format: 'png',
+};
+
+print(ui.Thumbnail({image: image.visualize(visualization), params: imageParams}));
