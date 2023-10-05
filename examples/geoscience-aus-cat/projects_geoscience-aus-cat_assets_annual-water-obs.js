7b8ed8420b51f76caa7e3dddda15026f57b1b6b1
Shirui Hao <shiruih@ngis.com.au>
2023-10-05 16:32:56 +1100

    Band names changed

--- examples/geoscience-aus-cat/projects_geoscience-aus-cat_assets_annual-water-obs.js
+++ examples/geoscience-aus-cat/projects_geoscience-aus-cat_assets_annual-water-obs.js
@@ -7,7 +7,7 @@ var image = water_obs.filterBounds(point)
                      .first();
                      
 var visualization_frequency = {
-  bands: ['water_frequency'],
+  bands: ['B03'],
   min: 0.0,
   max: 1.0,
   palette: ['ffffff', 'ffbbbb', '0000ff']
