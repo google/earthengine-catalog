c8a784a0171866ca07941c93a7f9b5eaa86b009d
Simon Ilyushchenko <simonf@google.com>
2023-10-20 09:43:27 -0700

    Add pixels read from a file to the NASA-vs-EE IMERG replica comparison and change the data range to show the diffs clearer.
    EE has the same data as the GDAL file
    
    PiperOrigin-RevId: 575244027

--- replicas/compare_gpm_half_hourly.py
+++ replicas/compare_gpm_half_hourly.py
@@ -12,6 +12,7 @@ Prerequsites:
 * xee (https://github.com/google/Xee) to use Xarray with the EE client.
 * pydap
 * numpy
+* osgeo.gdal
 
 Accounts needed:
 * NASA EarthData account (change 'username' and 'password' below to yours)
@@ -22,9 +23,15 @@ This script will run for about a minute.
 
 import ee
 import numpy as np
+from osgeo import gdal
 from pydap.cas.urs import setup_session
 from pydap.client import open_url
 import xarray
+
+# See
+# https://gpm1.gesdisc.eosdis.nasa.gov/data/GPM_L3/GPM_3IMERGHH.06/2000/153/3B-HHR.MS.MRG.3IMERG.20000601-S000000-E002959.0000.V06B.HDF5
+filename = 'HDF5:"/tmp/3B-HHR.MS.MRG.3IMERG.20000601-S000000-E002959.0000.V06B.HDF5"://Grid/precipitationCal'
+gdal_precip = gdal.Open(filename).ReadAsArray()
 
 ee.Initialize()
 
@@ -60,127 +67,134 @@ nasa_precip = nasa_dataset['hqprecip']
 # to avoid constantly refetching the bytes.
 first_nasa_precip = np.copy(nasa_precip[0, :, :])
 
+# Print the shape of the array read from GDAL
+print(gdal_precip.shape)
 # Print the shape of the first time slice of data from Earth Engine
 print(first_ee_precip.shape)
 # Print the shape of the rotated first time slice of data from OpenDAP
 print(np.rot90(first_nasa_precip[0]).shape)
-# Both print (3600, 1800)
+# All print (3600, 1800)
 
+gdal_normalized = np.flip(gdal_precip)
 # Flip verically the first time slice of data from Earth Engine
 # so that north is up
 ee_normalized = np.flip(ee_precip[0])
 # Rotate the first time slice of data from OpenDAP
 nasa_normalized = np.rot90(first_nasa_precip[0])
 
-# Select a specific range of data values from both datasets
-ee_subset = ee_normalized[500][800:1000].data
-nasa_subset = nasa_normalized[500][800:1000]
+# Select a specific range of data values from all datasets
+# TODO(simonf): I am confused why we need to reverse the list from GDAL.
+# The lon range in the file is -180 to 180
+gdal_subset = np.array(list(reversed(gdal_normalized[500])))[870:970]
+ee_subset = ee_normalized[500][870:970].data
+nasa_subset = nasa_normalized[500][870:970]
+
+np.set_printoptions(precision=6, floatmode='fixed')
+
+print('GDAL data from file')
+print(gdal_subset)
+print('Earth Engine data')
 print(ee_subset)
+print('NASA data')
 print(nasa_subset)
 
-# Compare the specific range of data values from both datasets
+# Compare a specific range of data values from all datasets
+print('NASA vs GDAL')
+print(nasa_subset == gdal_subset)
+print('NASA vs EE')
 print(nasa_subset == ee_subset)
+print('EE vs GDAL')
+print(ee_subset == gdal_subset)
 
-print(sum(nasa_subset == ee_subset))
-# This prints 196 because 4 out of 200 values are not the same.
+# This prints 96 when 4 out of 100 values are not the same.
 # See below for the full printout.
+print('NASA vs GDAL sum')
+print(sum(nasa_subset == ee_subset))
+print('NASA vs EE sum')
+print(sum(nasa_subset == ee_subset))
+print('EE vs GDAL sum')
+print(sum(ee_subset == gdal_subset))
 
 # pylint:disable=pointless-string-statement
 """
 Full output
 
+GDAL data from file
+[0.101135 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.133421 0.037022 0.049846 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.030000 0.000000 0.580000 0.590000 0.580000 0.500000 0.000000
+ 0.000000 0.000000 0.000000 0.550000 0.600000 0.600000 0.590000 0.530000
+ 0.470000 0.550000 0.540000 0.970000 0.970000 0.950000 0.800000 0.920000
+ 0.310000 0.310000 0.310000 0.050000 0.040000 0.040000 0.040000 0.000000
+ 0.050000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.020000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000]
 Earth Engine data
-
-[0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.22
- 0.21       0.21       0.52       0.32       0.32       0.34
- 0.14       0.14       0.         0.         0.         0.
- 0.48999998 0.64       0.64       0.87       1.02       0.66999996
- 0.24       0.07       0.         0.         0.         0.
- 0.         0.         0.         0.         0.10113539 0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.13342088 0.0370223  0.04984601 0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.03
- 0.         0.58       0.59       0.58       0.5        0.
- 0.         0.         0.         0.55       0.59999996 0.59999996
- 0.59       0.53       0.47       0.55       0.53999996 0.96999997
- 0.96999997 0.95       0.79999995 0.91999996 0.31       0.31
- 0.31       0.05       0.04       0.04       0.04       0.
- 0.05       0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.02       0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.        ]
-
+[0.101135 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.133421 0.037022 0.049846 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.030000 0.000000 0.580000 0.590000 0.580000 0.500000 0.000000
+ 0.000000 0.000000 0.000000 0.550000 0.600000 0.600000 0.590000 0.530000
+ 0.470000 0.550000 0.540000 0.970000 0.970000 0.950000 0.800000 0.920000
+ 0.310000 0.310000 0.310000 0.050000 0.040000 0.040000 0.040000 0.000000
+ 0.050000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.020000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000]
 NASA data
-
-[0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.22
- 0.21       0.21       0.52       0.32       0.32       0.34
- 0.14       0.14       0.         0.         0.         0.
- 0.48999998 0.64       0.64       0.87       1.02       0.66999996
- 0.24       0.07       0.         0.         0.         0.
- 0.         0.         0.         0.         0.05       0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.14       0.03       0.04       0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.03
- 0.         0.58       0.59       0.58       0.5        0.
- 0.         0.         0.         0.55       0.59999996 0.59999996
- 0.59       0.53       0.47       0.55       0.53999996 0.96999997
- 0.96999997 0.95       0.79999995 0.91999996 0.31       0.31
- 0.31       0.05       0.04       0.04       0.04       0.
- 0.05       0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.02       0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.         0.         0.         0.         0.
- 0.         0.        ]
-
-Comparison
-
+[0.050000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.140000 0.030000 0.040000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.030000 0.000000 0.580000 0.590000 0.580000 0.500000 0.000000
+ 0.000000 0.000000 0.000000 0.550000 0.600000 0.600000 0.590000 0.530000
+ 0.470000 0.550000 0.540000 0.970000 0.970000 0.950000 0.800000 0.920000
+ 0.310000 0.310000 0.310000 0.050000 0.040000 0.040000 0.040000 0.000000
+ 0.050000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.020000
+ 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+ 0.000000 0.000000 0.000000 0.000000]
+NASA vs GDAL
+[False  True  True  True  True  True  True  True  True False False False
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True]
+NASA vs EE
+[False  True  True  True  True  True  True  True  True False False False
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True  True  True  True  True  True  True  True  True
+  True  True  True  True]
+EE vs GDAL
 [ True  True  True  True  True  True  True  True  True  True  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True False  True
-  True  True  True  True  True  True  True False False False  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
   True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True  True  True  True  True
-  True  True  True  True  True  True  True  True]
-
+  True  True  True  True]
+NASA vs GDAL sum
+96
+NASA vs EE sum
+96
+EE vs GDAL sum
+100
 """
