0cb0a913d9a25ff4a33225ee36a3fdfaa8793cb1
Google Earth Engine Community Authors <noreply@google.com>
2023-10-30 09:44:56 -0700

    Update code example for GOOGLE/CLOUD_SCORE_PLUS/V1/S2_HARMONIZED
    PiperOrigin-RevId: 577873794

--- examples/GOOGLE/GOOGLE_CLOUD_SCORE_PLUS_V1_S2_HARMONIZED.js
+++ examples/GOOGLE/GOOGLE_CLOUD_SCORE_PLUS_V1_S2_HARMONIZED.js
@@ -1,45 +1,32 @@
-// Region of interest.
-var roi = ee.Geometry.Rectangle([-124.8697, 35.5806, -119.9917, 39.9554]);
+// Harmonized Sentinel-2 Level 2A collection.
+var s2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED');
 
 // Cloud Score+ image collection. Note Cloud Score+ is produced from Sentinel-2
 // Level 1C data and can be applied to either L1C or L2A collections.
 var csPlus = ee.ImageCollection('GOOGLE/CLOUD_SCORE_PLUS/V1/S2_HARMONIZED');
 
-// Harmonized Sentinel-2 Level 2A collection.
-var s2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED');
+// Region of interest.
+var ROI = ee.Geometry.Point(-119.9087, 37.4159);
 
-// Link S2 and CS+ results.
-var linkedCollection = s2.linkCollection(csPlus, ['cs', 'cs_cdf']);
+// Use 'cs' or 'cs_cdf', depending on your use case; see docs for guidance.
+var QA_BAND = 'cs_cdf';
 
-// Date range for median composite.
-var dateStart = '2023-06-01';
-var dateEnd = '2023-07-01';
+// The threshold for masking; values between 0.50 and 0.65 generally work well.
+// Higher values will remove thin clouds, haze & cirrus shadows.
+var CLEAR_THRESHOLD = 0.60;
 
-// Masks pixels with low CS+ QA scores.
-function maskLowQA(image) {
-  // CS+ QA band to use for masking. Options are 'cs' and 'cs_cdf'.
-  var qaBand = 'cs';
-
-  // Adjustable threshold for converting CS+ QA to a binary mask.
-  // Higher thresholds will mask out partial occlusions like thin clouds, haze &
-  // cirrus shadows. Lower thresholds will be more permissive of these
-  // occlusions.
-  var clearThreshold = 0.60;
-  var mask = image.select(qaBand).gte(clearThreshold);
-
-  return image.updateMask(mask);
-}
-
-
-// Filter collection, mask pixels with low QA scores, and generate clear median
-// composite.
-var composite = linkedCollection.filterBounds(roi)
-                    .filterDate(dateStart, dateEnd)
-                    .map(maskLowQA)
-                    .median();
+// Make a clear median composite.
+var composite = s2
+    .filterBounds(ROI)
+    .filterDate('2023-01-01', '2023-02-01')
+    .linkCollection(csPlus, [QA_BAND])
+    .map(function(img) {
+      return img.updateMask(img.select(QA_BAND).gte(CLEAR_THRESHOLD));
+    })
+    .median();
 
 // Sentinel-2 visualization parameters.
-var s2Viz = {bands: ['B4', 'B3', 'B2'], min: 0, max: 3000};
+var s2Viz = {bands: ['B4', 'B3', 'B2'], min: 0, max: 2500};
 
 Map.addLayer(composite, s2Viz, 'median composite');
-Map.centerObject(roi, 11);
+Map.centerObject(ROI, 11);
