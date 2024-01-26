a9da4d703512c52e221a93499c4bd603b41e59a9
Simon Ilyushchenko <simonf@google.com>
2024-01-26 12:16:19 -0800

    Make collection and catalog title checks errors more self-explanatory.
    Allow collection titles to start with lowercase letters - geoBoundaries is an example where this is okay, as that's the verbatim original name. Stop special-casing ISDSOIL dataset titles, which are now valid.
    
    PiperOrigin-RevId: 601834214

--- checker/node/title_test.py
+++ checker/node/title_test.py
@@ -32,12 +32,29 @@ class CatalogTest(test_utils.NodeTest):
   def test_is_str(self):
     self.assert_catalog({TITLE: ['not a str']}, '"title" must be a str')
 
+  def test_invalid_too_short(self):
+    dataset_id = 'a'
+    self.assert_catalog(
+        {TITLE: dataset_id},
+        'Catalog title is too short: "a"',
+        dataset_id=dataset_id,
+    )
+
+  def test_invalid_too_long(self):
+    dataset_id = 'a' * 31
+    self.assert_catalog(
+        {TITLE: dataset_id},
+        'Catalog title is too long: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"',
+        dataset_id=dataset_id,
+    )
+
   def test_invalid(self):
     dataset_id = 'not a valid title'
     self.assert_catalog(
         {TITLE: dataset_id},
-        'Catalog title not valid: "not a valid title"',
-        dataset_id=dataset_id)
+        'Catalog title has invalid characters: "not a valid title"',
+        dataset_id=dataset_id,
+    )
 
   def test_warning_for_title_not_being_last_id_component(self):
     a_title = 'A_title'
@@ -70,13 +87,23 @@ class CollectionTest(test_utils.NodeTest):
   def test_invalid(self):
     self.assert_collection(
         {TITLE: '% not a valid title'},
-        'Collection title not valid: "% not a valid title"')
+        'Collection title has invalid characters: "% not a valid title"',
+    )
+
+  def test_invalid_too_short(self):
+    dataset_id = 'a'
+    self.assert_collection(
+        {TITLE: dataset_id},
+        'Collection title is too short: "a"',
+        dataset_id=dataset_id,
+    )
 
   def test_invalid_too_long(self):
     extra = 'Z' * 140
     self.assert_collection(
         {TITLE: 'Too long ' + extra},
-        f'Collection title not valid: "Too long {extra}"')
+        f'Collection title is too long: "Too long {extra}"',
+    )
 
 
 if __name__ == '__main__':
