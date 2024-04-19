25a88087f4a38a5922f2081c4198dda6b79652aa
Simon Ilyushchenko <simonf@google.com>
2024-04-19 10:07:12 -0700

    Use filterDate() instead of mosaic() for the updated JRC 2020 forest baseline, as we reingested the 2020 data as a single asset instead of a tile mosaic.
    PiperOrigin-RevId: 626398340

--- examples/JRC/JRC_GFC2020_V1.js
+++ examples/JRC/JRC_GFC2020_V1.js
@@ -1,4 +1,5 @@
-var dataset = ee.ImageCollection('JRC/GFC2020/V1').mosaic();
+var image2020 = ee.ImageCollection('JRC/GFC2020/V1').filterDate(
+  '2020-12-31').first();
 
 var visualization = {
   bands: ['Map'],
@@ -6,4 +7,4 @@ var visualization = {
 
 Map.setCenter(0.0, 0.0, 2);
 
-Map.addLayer(dataset, visualization, 'EC JRC Global forest cover 2020 – V1');
+Map.addLayer(image2020, visualization, 'EC JRC Global forest cover 2020 – V1');
--- examples/JRC/JRC_GFC2020_V1_preview.js
+++ examples/JRC/JRC_GFC2020_V1_preview.js
@@ -7,15 +7,16 @@ var dataVisualization = {
   bands: ['Map'],
   palette: ['4D9221']};
 
-var dataset =
-    ee.ImageCollection('JRC/GFC2020/V1').mosaic().visualize(dataVisualization);
+var image2020 =
+    ee.ImageCollection('JRC/GFC2020/V1').filterDate(
+      '2020-12-31').first().visualize(dataVisualization);
 
 var imageWithBackground =
-    ee.ImageCollection([waterLandBackground, dataset]).mosaic();
+    ee.ImageCollection([waterLandBackground, image2020]).mosaic();
 
 Map.setCenter(0.0, 0.0, 2);
 
-Map.addLayer(dataset, {}, 'EC JRC Global forest cover 2020 – V1');
+Map.addLayer(image2020, {}, 'EC JRC Global forest cover 2020 – V1');
 
 var lon = -70;
 var lat = -10;
