15ffeaf0db11842198e95aeb20695b1196b0d07a
Simon Ilyushchenko <simonf@google.com>
2024-07-07 19:09:05 -0700

    Unindex pre-Collection Landsat datasets from the catalog and remove a few related examples
    PiperOrigin-RevId: 650092009

--- examples/LANDSAT/LANDSAT_LE7_L1T_ANNUAL_GREENEST_TOA.js
+++
@@ -1,6 +0,0 @@
-// NOTE: This ImageCollection is deprecated.
-
-var dataset = ee.ImageCollection('LANDSAT/LE7_L1T_ANNUAL_GREENEST_TOA');
-
-Map.setCenter( -119.0446, 35.1195, 12);
-Map.addLayer(dataset.select('greenness'), null, 'greenness');
