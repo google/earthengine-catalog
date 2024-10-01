1e97e6e6bac424747f7dd35b1c0f136d1e6f58a3
jheyer23 <joshua.heyer@usda.gov>
2024-10-01 15:10:20 -0600

    Adding Hawaii and Puerto Rico US Virgin Islands information to LCMS data catalog

--- examples/USFS/USFS_GTAC_LCMS_v2023-9.js
+++ examples/USFS/USFS_GTAC_LCMS_v2023-9.js
@@ -1,7 +1,7 @@
 var dataset = ee.ImageCollection('USFS/GTAC/LCMS/v2023-9');
 
 var lcms = dataset.filterDate('2021', '2022')  // range: [1985, 2023]
-               .filter('study_area == "CONUS"')  // or "SEAK" ("PRUSVI" and "HAWAII" available late summer 2024)
+               .filter('study_area == "CONUS"')  // or "SEAK"; "PRUSVI"; "HAWAII" 
                .first();
 
 Map.addLayer(lcms.select('Land_Cover'), {}, 'Land Cover');
