672ec0457a497b63f0bb2d6dd44374e2be017e06
Simon Ilyushchenko <simonf@google.com>
2024-11-06 14:53:11 -0800

    Fix JRC forest type example code as requested by data provider
    PiperOrigin-RevId: 693872473

--- examples/JRC/JRC_GFC2020_subtypes_V0.js
+++ examples/JRC/JRC_GFC2020_subtypes_V0.js
@@ -19,7 +19,7 @@ var PALETTE =
     // value 10 - Primary forest
     // value 20 - Planted/Plantation forest
     var GFT2020 = ee.ImageCollection('JRC/GFC2020_subtypes/V0').mosaic()
-GFT2020 = GFT2020.updateMask(GFT2020.gte(1))
 Map.addLayer(
     GFT2020, {min: 0, max: 20, palette: PALETTE},
     'EC JRC Global forest types 2020 – V0')
+Map.setOptions('SATELLITE')
