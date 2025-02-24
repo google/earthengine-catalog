9153025f3e6edc67cc634949140b2af8a1d93d3e
Mauricio van den Berg <mauricio.vandenberg@thekids.org.au>
2025-02-24 14:45:28 +0800

    Add corresponding examples

---
+++ examples/MalariaAtlasProject/MalariaAtlasProject_EVI_1km_Annual.js
@@ -0,0 +1,10 @@
+var dataset = ee.ImageCollection('projects/covariate/assets/EVI_v061/1km/Annual')
+                  .filter(ee.Filter.date('2018-01-01', '2023-01-01'));
+var means = dataset.select('Mean');
+var visParams = {
+  min: 0.0,
+  max: 1.0,
+  palette: ['ffffff','fcd163','99b718','66a000','3e8601','207401','056201','004c00','011301'],
+};
+Map.setCenter(0, 0, 2);
+Map.addLayer(means, visParams, 'Annual EVI: Malaria Atlas Project Gap-Filled Enhanced Vegetation Index');
