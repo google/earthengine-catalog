48b556803df617dd649df24d266a60bbc0ec98cc
Tom Melendez <supertom@methanesat.org>
2025-03-14 17:06:57 +0000

    add comment to examples and update owners to use request form

--- examples/edf-methanesat-ee/projects_edf-methanesat-ee_assets_public-preview_L4point_FeatureView.js
+++ examples/edf-methanesat-ee/projects_edf-methanesat-ee_assets_public-preview_L4point_FeatureView.js
@@ -1,3 +1,4 @@
+// Request access to this data by filling out the form at: https://forms.gle/jqw4Mvr63dsV1fUF8
 var fvLayer = ui.Map.FeatureViewLayer('projects/edf-methanesat-ee/assets/public-preview/L4point_FeatureView');
 
 var visParams = {
