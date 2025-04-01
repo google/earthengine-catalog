4d612ecbb41ce7d71bee56366991852b449322de
Tom Melendez <supertom@methanesat.org>
2025-04-01 01:11:49 +0000

    MethaneSAT Example Updates * L4area uses color palette very similar to Data Portal palette * L3concentration preview focuses on one scene * L4area preview uses updated color palette and focuses on one scene.

--- examples/edf-methanesat-ee/projects_edf-methanesat-ee_assets_public-preview_L3concentration_preview.js
+++ examples/edf-methanesat-ee/projects_edf-methanesat-ee_assets_public-preview_L3concentration_preview.js
@@ -1,6 +1,6 @@
 // Request access to this data by filling out the form at: https://forms.gle/jqw4Mvr63dsV1fUF8
 var methaneSATArea = ee.ImageCollection("projects/edf-methanesat-ee/assets/public-preview/L3concentration")
-  .filterDate('2024-06-01', '2025-01-31');
+  .filterDate('2024-12-01', '2024-12-26');
 
 // Visualization
 var datasetVis = methaneSATArea.mosaic().visualize({
@@ -15,7 +15,7 @@ var background = ee.Image.rgb(256, 256, 256).rename('vis-red', 'vis-green', 'vis
 var imageWithBackground = background.blend(datasetVis);
 
 // Visualizing
-var region = ee.Geometry.Rectangle(-104.8, 30.3, -100.8, 33.5);
+var region = ee.Geometry.Rectangle(-95.593, 30.626, -92.681, 33.739);
 var pixels = 256;
 var imageParams = {
   dimensions: [pixels, pixels],
