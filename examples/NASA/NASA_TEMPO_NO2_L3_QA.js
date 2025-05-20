8c042a38d526739b196237ca70f8aa2888c8fc3e
Google Earth Engine Community Authors <noreply@google.com>
2025-05-20 02:01:03 -0700

    Refactor the TEMPO L3 NO2 daily data (DD) code to improve code reusability. Also, add DD for collecting NO2 quality QA collection.
    PiperOrigin-RevId: 760980412

---
+++ examples/NASA/NASA_TEMPO_NO2_L3_QA.js
@@ -0,0 +1,14 @@
+var collection = ee.ImageCollection('NASA/TEMPO/NO2_L3_QA')
+    .filterDate('2024-04-01', '2024-04-05')
+
+var visParams = {
+  min: 0,
+  max: 1.5e16,
+  bands: ['vertical_column_troposphere'],
+  palette: [
+    '000080', '0000D9', '4000FF', '8000FF', '0080FF',
+    '00D9FF', '80FFFF', 'FF8080', 'D90000', '800000'
+  ]
+};
+Map.setCenter(-95.06, 42.02, 3)
+Map.addLayer(collection, visParams, 'Tropospheric NO2')
