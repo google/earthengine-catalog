045bfe27b6b49aeedf3cb2b7c4ca48af7e5725b9
Dan Pencoske <dpencosk@google.com>
2025-06-03 08:53:07 -0700

    Add a first-cut dataset description for NOAA/VIIRS/AOD_EDR_V3.
    PiperOrigin-RevId: 766684811

--- checker/node/examples.py
+++ checker/node/examples.py
@@ -861,6 +861,10 @@ class Check(stac.NodeCheck):
   def run(cls, node: stac.Node) -> Iterator[stac.Issue]:
     if node.type == stac.StacType.CATALOG: return
     if node.id.startswith('TEMPLATE'): return
+    if node.stac.get(stac.GEE_STATUS) in (
+        stac.Status.DEPRECATED, stac.Status.INCOMPLETE
+    ):
+      return
 
     # CCDC is not planned to be made public so far, so it has
     # no examples.
