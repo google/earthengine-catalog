388e8db62c49fa879b00fb8aa91b232e4fa77ab2
Google Earth Engine Community Authors <noreply@google.com>
2025-06-13 18:28:13 -0700

    Add GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL to the Earth Engine data catalog.
    PiperOrigin-RevId: 771294064

---
+++ examples/GOOGLE/GOOGLE_SATELLITE_EMBEDDING_V1_ANNUAL.js
@@ -0,0 +1,40 @@
+// Load collection.
+var dataset = ee.ImageCollection('GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL');
+
+// Point of interest.
+var point = ee.Geometry.Point(-120.5122, 37.3596);
+
+// Get embedding images for two years.
+var image1 = dataset
+      .filterDate('2023-01-01', '2024-01-01')
+      .filterBounds(point)
+      .first();
+
+var image2 = dataset
+      .filterDate('2024-01-01', '2025-01-01')
+      .filterBounds(point)
+      .first();
+
+// Visualize three axes of the emedding space as an RGB.
+var visParams = {min: -0.3, max: 0.3, bands: ['A01', 'A16', 'A09']};
+
+Map.addLayer(image1, visParams, '2023 embeddings');
+Map.addLayer(image2, visParams, '2024 embeddings');
+
+// Calculate dot product (cosine similarity) to highlight deviations
+// between embedding vectors.
+var dotProd = image1
+    .multiply(image2)
+    .reduce(ee.Reducer.sum())
+    .multiply(-0.5) // scale, and flip sign so positive = more different
+    .add(0.5);      // shift so that 0 = no change, 1 = change
+
+// Add dot product to the map.
+Map.addLayer(
+  dotProd,
+  {min: 0, max: 0.3},
+  'Change between years (brighter=larger deviation)'
+);
+
+Map.centerObject(point, 12);
+Map.setOptions('SATELLITE');
---
+++ examples/GOOGLE/GOOGLE_SATELLITE_EMBEDDING_V1_ANNUAL_preview.js
@@ -0,0 +1,33 @@
+// Load collection.
+var dataset = ee.ImageCollection("GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL");
+
+// Year of interest.
+var year = 2024;
+
+// Region of interest.
+var roi = ee.Geometry.Rectangle([-74.3, 40.5, -73.7, 41], null, false);
+
+var image = dataset
+          .filterDate(year.toString() + "-01-01", (year + 1).toString() + "-01-01")
+          .filterBounds(roi)
+          .first();
+
+var pixels = 256;
+
+var imageParams = {
+  dimensions: [pixels, pixels],
+  region: roi,
+  crs: 'EPSG:4326',
+  format: 'png',
+};
+
+var thumbnailImage = image.visualize({
+  min: -0.25,
+  max:0.25,
+  bands: ['A03', 'A02', 'A04']
+});
+
+print(ui.Thumbnail({
+  image: thumbnailImage,
+  params: imageParams,
+}));
