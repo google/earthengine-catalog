3ff13588cfef2bdcec7bfabfda8418e8ac1422f6
Google Earth Engine Community Authors <noreply@google.com>
2025-06-17 13:35:56 -0700

    Nature Trace addition.
    PiperOrigin-RevId: 772600935

---
+++ examples/nature-trace/projects_nature-trace_assets_forest_typology_natural_forest_2020_v1_0_collection.js
@@ -0,0 +1,15 @@
+var probabilities =
+    ee.ImageCollection(
+          'projects/nature-trace/assets/forest_typology/natural_forest_2020_v1_0_collection')
+        .mosaic()
+        .select('B0')
+        .divide(250);
+
+Map.addLayer(
+    probabilities.mask(probabilities.neq(0)),
+    {min: 0, max: 1, palette: ['white', 'green']},
+    'Natural forest probabilities');
+
+Map.addLayer(
+    probabilities.gte(0.5).mask(probabilities.gte(0.5)), {palette: 'teal'},
+    'Natural forest map at threshold');
