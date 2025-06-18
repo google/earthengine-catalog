b5096c9df2c7c948f0f034119abf1b5f6b5bdc79
Google Earth Engine Community Authors <noreply@google.com>
2025-06-18 13:15:45 -0700

    Fix error in in forest data partnership demo coffee script
    PiperOrigin-RevId: 773035644

--- examples/forestdatapartnership/projects_forestdatapartnership_assets_coffee_model_2025a.js
+++ examples/forestdatapartnership/projects_forestdatapartnership_assets_coffee_model_2025a.js
@@ -3,10 +3,10 @@ Map.setCenter(-88.84, 14.57, 12);
 var collection = ee.ImageCollection(
   'projects/forestdatapartnership/assets/coffee/model_2025a');
 
-var cocoa2020 = collection.filterDate('2020-01-01', '2020-12-31').mosaic().gt(0.95);
+var coffee2020 = collection.filterDate('2020-01-01', '2020-12-31').mosaic().gt(0.95);
 Map.addLayer(
-  cocoa2020.selfMask(), {min: 0.5, max: 1, palette: 'white,brown'}, 'coffee 2020');
+  coffee2020.selfMask(), {min: 0.5, max: 1, palette: 'white,brown'}, 'coffee 2020');
 
-var cocoa2023 = collection.filterDate('2023-01-01', '2023-12-31').mosaic().gt(0.95);
+var coffee2023 = collection.filterDate('2023-01-01', '2023-12-31').mosaic().gt(0.95);
 Map.addLayer(
-  cocoa2023.selfMask(), {min: 0.5, max: 1, palette: 'white,green'}, 'coffee 2023');
+  coffee2023.selfMask(), {min: 0.5, max: 1, palette: 'white,green'}, 'coffee 2023');
