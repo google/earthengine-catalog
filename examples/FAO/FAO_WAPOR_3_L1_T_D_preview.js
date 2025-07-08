bd0fc6e2a129fa15906aa06ff5ae91fe3a9d706e
Google Earth Engine Community Authors <noreply@google.com>
2025-07-08 11:02:30 -0700

    Adding FAO/WAPOR_3 dataset descriptions
    PiperOrigin-RevId: 780635023

---
+++ examples/FAO/FAO_WAPOR_3_L1_T_D_preview.js
@@ -0,0 +1,35 @@
+// FAO_WAPOR_3_L1_T_D_preview.js
+var lon = 17.5;
+var lat = 0;
+var delta = 37;
+var areaOfInterest = ee.Geometry.Rectangle(
+    [lon - delta, lat - delta, lon + delta, lat + delta], null, false);
+
+var dataset = ee.ImageCollection('FAO/WAPOR/3/L1_T_D').first();
+var visParams = {min: 0, max: 50};
+
+var imageToDisplay = dataset.visualize(visParams).clip(areaOfInterest);
+
+
+// Background
+var waterLand = ee.Image('NOAA/NGDC/ETOPO1').select('bedrock').gt(0.0);
+var waterLandBackground =
+    waterLand.visualize({palette: ['cadetblue', 'lightgray']});
+
+var blendedLayer = waterLandBackground.blend(imageToDisplay);
+Map.addLayer(blendedLayer, {}, 'L1_T_D Preview');
+
+Map.setCenter(lon, lat, 4);
+
+var pixels = 256;
+var thumbParams = {
+  dimensions: [pixels, pixels],
+  region: areaOfInterest,
+  crs: 'EPSG:3857',
+  format: 'png',
+};
+
+print(ui.Thumbnail({
+  image: blendedLayer,
+  params: thumbParams,
+}));
