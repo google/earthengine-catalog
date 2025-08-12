87ff025a535d8687874c0104daf70d24293b711c
Google Earth Engine Community Authors <noreply@google.com>
2025-08-12 06:47:17 -0700

    Data description for JRC_CEMS_GLOFAS_FloodHazard_v2
    PiperOrigin-RevId: 794089392

---
+++ examples/JRC/JRC_CEMS_GLOFAS_FloodHazard_v2_preview.js
@@ -0,0 +1,42 @@
+var dataset = ee.ImageCollection('JRC/CEMS_GLOFAS/FloodHazard/v2');
+var temp = dataset.select('RP100_depth').mean();
+var tempVis = {
+  min: 0,
+  max: 1,
+  palette: ['ffffff','0000ff'],
+};
+
+var lon = -86.47;
+var lat = 47.28;
+
+var waterLand = ee.Image('NOAA/NGDC/ETOPO1').select('bedrock').gt(0.0);
+var backgroundPalette = ['cadetblue', 'lightgray'];
+var waterLandVis = {palette: backgroundPalette};
+var waterLandBackground = waterLand.visualize({palette: backgroundPalette});
+
+var gray = 150;
+var background = ee.Image.rgb(gray, gray, gray).visualize({min: 0, max: 255});
+
+Map.setCenter(lon, lat, 4);
+Map.addLayer(temp, tempVis, 'RP100_depth');
+
+// Degrees in EPSG:3857.
+var delta = 15;
+// Width and height of the thumbnail image.
+var pixels = 256;
+
+var image = temp.visualize(tempVis);
+var imageWithBackground = ee.ImageCollection([waterLandBackground, image]).mosaic();
+
+var areaOfInterest = ee.Geometry.Rectangle(
+    [lon - delta, lat - delta, lon + delta, lat + delta], null, false);
+
+var imageParams = {
+  dimensions: [pixels, pixels],
+  region: areaOfInterest,
+  crs: 'EPSG:3857',
+  format: 'png',
+};
+
+
+print(ui.Thumbnail({image: imageWithBackground, params: imageParams}));
