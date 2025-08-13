69a460463050dbbe69475c95f9ea5854de9dc150
Simon Ilyushchenko <simonf@simonf.com>
2025-08-13 10:29:22 -0700

    Update projects_malariaatlasproject_assets_EVI_v061_1km_Monthly_preview.js

--- examples/malariaatlasproject/projects_malariaatlasproject_assets_EVI_v061_1km_Monthly_preview.js
+++ examples/malariaatlasproject/projects_malariaatlasproject_assets_EVI_v061_1km_Monthly_preview.js
@@ -1,6 +1,6 @@
 var dataset = ee.ImageCollection('projects/malariaatlasproject/assets/EVI_v061/1km/Monthly')
                   .filter(ee.Filter.date('2022-01-01', '2022-12-31'));
-var means = dataset.select('Mean');
+var means = dataset.select('Mean').mosaic();
 var palette = ['ffffff','fcd163','99b718','66a000','3e8601','207401','056201','004c00','011301'];
 var visParams = {
   min: 0.0,
