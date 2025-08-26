e8cec85182fb442492b02a710bca9667ae5b2a74
leandroleal <leal.parente@gmail.com>
2025-08-26 11:16:25 +0200

    add svh 2024 code snippet

--- examples/global-pasture-watch/projects_global-pasture-watch_assets_gsvh-30m_v1_short-veg-height_m.js
+++ examples/global-pasture-watch/projects_global-pasture-watch_assets_gsvh-30m_v1_short-veg-height_m.js
@@ -9,7 +9,7 @@ var svh = ee.ImageCollection(
     "projects/global-pasture-watch/assets/gsvh-30m/v1/short-veg-height_m"
 )
 
-var svh2022 = svh.filterDate('2022-01-01', '2023-01-01').first().multiply(SCALE_FACTOR);
+var svh2022 = svh.filterDate('2024-01-01', '2025-01-01').first().multiply(SCALE_FACTOR);
 Map.addLayer(svh2022, svhVis, 'Median vegetation height (2024)');
 
 var svh2000 = svh.filterDate('2000-01-01', '2001-01-01').first().multiply(SCALE_FACTOR);
