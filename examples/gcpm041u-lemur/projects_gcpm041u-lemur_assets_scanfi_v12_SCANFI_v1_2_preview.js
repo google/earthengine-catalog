3c91fcd19e9a58dc67b8ae064e87231f0737296b
Philippe Villemaire <philippe.villemaire@gcp.nrcan-rncan.gc.ca>
2025-10-01 15:19:01 -0400

    added thumbnail creation script

--- examples/gcpm041u-lemur/projects_gcpm041u-lemur_assets_scanfi_v12_SCANFI_v1_2_preview.js
+++ examples/gcpm041u-lemur/projects_gcpm041u-lemur_assets_scanfi_v12_SCANFI_v1_2_preview.js
@@ -2,9 +2,37 @@ var dataset = ee.Image("projects/gcpm041u-lemur/assets/scanfi_v12/SCANFI_v1_2");
 var balsamFir = dataset.select('balsamFir').selfMask();
 var vis = {
   min: 0,
-  max: 100,
-  palette: ['ffffff', '05ff09']
+  max: 75,
+  palette: [
+    'yellow', 'darkgreen'
+  ]
 };
-Map.setCenter(-94, 56, 5);
-Map.addLayer(balsamFir, vis, 'Balsam Fir Percentage')
 
+
+var lat = 48;
+var lon = -60;
+
+Map.setCenter(lon, lat, 5);
+
+// Degrees in EPSG:3857.
+var deltax = 8;
+var deltay = 5;
+// Width and Height of the thumbnail image.
+var pixels = 256;
+
+
+var areaOfInterest = ee.Geometry.Rectangle(
+  [lon - deltax, lat - deltay, lon + deltax, lat + deltay], null, false);
+
+var visParams = {
+  dimensions: [pixels, pixels],
+  region: areaOfInterest, crs: 'EPSG:3857', format: 'png'};
+
+var image = balsamFir.visualize(vis);
+var imageClip = balsamFir.visualize(vis).clip(areaOfInterest);
+
+Map.addLayer(balsamFir, null, 'Balsam Fir Percentage');
+
+Map.addLayer(areaOfInterest, {color: 'red', opacity: 0.1}, 'Area of Interest');
+
+print(ui.Thumbnail({image: imageClip, params: visParams}));
