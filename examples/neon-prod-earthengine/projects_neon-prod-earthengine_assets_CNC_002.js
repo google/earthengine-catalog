18cce16c4ee2745608aee16fb87a4ff482064c6f
Bridget Hass <bhass@battelleecology.org>
2026-03-04 12:54:45 -0700

    update example script to include uncertainty layer, add thumbnail script

--- examples/neon-prod-earthengine/projects_neon-prod-earthengine_assets_CNC_002.js
+++ examples/neon-prod-earthengine/projects_neon-prod-earthengine_assets_CNC_002.js
@@ -3,19 +3,25 @@ var cnc = ee.ImageCollection(
   'projects/neon-prod-earthengine/assets/CNC/002');
 
 // Display available images in the CNC Image Collection
-print('NEON CNC Images', cnc.aggregate_array('system:index'))
+print('NEON Canopy Nitrogen Images')
+print(cnc.aggregate_array('system:index'))
 
 // Filter by specific image index
-var selected_image = cnc.filter(ee.Filter.eq('system:index', '2019_HARV_6')).first();
+var cnc_harv2019 = cnc.filter(ee.Filter.eq('system:index', '2019_HARV_6')).first();
 
-// Mask nitrogen band using nitrogen_valid band
-var nit_valid = selected_image.select('nitrogen_valid');
-selected_image = selected_image.updateMask(nit_valid);
+// Mask nitrogen band using valid pixel band
+var cnc_valid = cnc_harv2019.select('Valid_Pixel_Classification');
+var cnc_masked = cnc_harv2019.updateMask(cnc_valid);
+
+// Read in the uncertainty band
+var cnc_uncertainty = cnc_harv2019.select('Nitrogen_Uncertainty');
 
 // Define the visualization parameters
-// Note: The min/max values below use a 95% stretch (2.5-97.5 percentile)
-var visParams = {bands: ['nitrogen'], min: 1.32, max: 2.23, palette: ['darkblue', 'blue', 'cyan', 'yellow', 'orange', 'white']};
+// Note: The min/max values below use a 98% stretch
+var visParamsNit = {bands: ['Nitrogen_Percent'], min: 1.38, max: 2.76, palette: ['#440154', '#3b528b', '#21908c', '#5dc963', '#fde725']};
+var visParamsUnc = {min: 0.44, max: 1.5, palette: ['#0d0887', '#7e03a8', '#cc4778', '#f89540', '#f0f921']};
 
-// Add the layer to the map and center on the site
-Map.addLayer(selected_image, visParams, 'HARV 2019 Nitrogen (%)');
-Map.centerObject(selected_image);
+// Add the nitrogen and uncertainty layers to the map and center on the site
+// Note that the masked areas in the nitrogen layer have higher uncertainty
+Map.addLayer(cnc_uncertainty, visParamsUnc, 'HARV 2019 Nitrogen Uncertainty (%)');
+Map.addLayer(cnc_masked, visParamsNit, 'HARV 2019 Nitrogen (%)');
