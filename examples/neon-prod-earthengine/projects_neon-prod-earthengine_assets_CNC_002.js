8055efe7e683417bf3c02227c55721662fe35ffd
Bridget Hass <bhass@battelleecology.org>
2026-03-04 14:42:20 -0700

    mask uncertainty layer as well in the uncertainty script

--- examples/neon-prod-earthengine/projects_neon-prod-earthengine_assets_CNC_002.js
+++ examples/neon-prod-earthengine/projects_neon-prod-earthengine_assets_CNC_002.js
@@ -13,15 +13,16 @@ var cnc_harv2019 = cnc.filter(ee.Filter.eq('system:index', '2019_HARV_6')).first
 var cnc_valid = cnc_harv2019.select('Valid_Pixel_Classification');
 var cnc_masked = cnc_harv2019.updateMask(cnc_valid);
 
-// Read in the uncertainty band
-var cnc_uncertainty = cnc_harv2019.select('Nitrogen_Uncertainty');
+// Read in the uncertainty band and mask using valid pixel band
+var cnc_unc = cnc_harv2019.select('Nitrogen_Uncertainty');
+var cnc_unc_masked = cnc_unc.updateMask(cnc_valid);
 
 // Define the visualization parameters
-// Note: The min/max values below use a 98% stretch
-var visParamsNit = {bands: ['Nitrogen_Percent'], min: 1.38, max: 2.76, palette: ['#440154', '#3b528b', '#21908c', '#5dc963', '#fde725']};
-var visParamsUnc = {min: 0.44, max: 1.5, palette: ['#0d0887', '#7e03a8', '#cc4778', '#f89540', '#f0f921']}; 
+// The min/max values below use a 2 sigma (~95%) stretch
+var visParamsNit = {bands: ['Nitrogen_Percent'], min: 1.6, max: 2.14, palette: ['#440154', '#3b528b', '#21908c', '#5dc963', '#fde725']};
+var visParamsUnc = {min: 0.4, max: 0.7, palette: ['#0d0887', '#7e03a8', '#cc4778', '#f89540', '#f0f921']};
 
-// Add the nitrogen and uncertainty layers to the map and center on the site
-// Note that the masked areas in the nitrogen layer have higher uncertainty
-Map.addLayer(cnc_uncertainty, visParamsUnc, 'HARV 2019 Nitrogen Uncertainty (%)');
+// Add the masked nitrogen and uncertainty layers to the map and center on the site
+Map.addLayer(cnc_unc_masked, visParamsUnc, 'HARV 2019 Nitrogen Uncertainty (%)');
 Map.addLayer(cnc_masked, visParamsNit, 'HARV 2019 Nitrogen (%)');
+Map.centerObject(cnc_harv2019, 11);
