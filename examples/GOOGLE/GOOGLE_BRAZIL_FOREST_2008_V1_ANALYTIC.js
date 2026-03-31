beca49f55414d7c3c4155796b7c1228a96493f8c
Justin Braaten <braaten@google.com>
2026-03-31 13:23:59 -0700

    Update visualization parameters for GOOGLE/BRAZIL_FOREST_2008/V1/ANALYTIC example
    PiperOrigin-RevId: 892509861

--- examples/GOOGLE/GOOGLE_BRAZIL_FOREST_2008_V1_ANALYTIC.js
+++ examples/GOOGLE/GOOGLE_BRAZIL_FOREST_2008_V1_ANALYTIC.js
@@ -1,16 +1,22 @@
-var analytic = ee.Image("GOOGLE/BRAZIL_FOREST_2008/V1/ANALYTIC");
+var analytic = ee.Image('GOOGLE/BRAZIL_FOREST_2008/V1/ANALYTIC');
 
-// Calculate NDVI using the Red (R) and Near-Infrared (N) bands
+// Calculate NDVI using the Red (R) and Near-Infrared (N) bands.
 var ndvi = analytic.normalizedDifference(['N', 'R']).rename('NDVI');
 
 var ndviParams = {
   min: 0,
-  max: 0.8,
+  max: 0.75,
   palette: ['white', 'green']
 };
 
 Map.setCenter(-55.0, -10.0, 6);
 Map.addLayer(ndvi, ndviParams, 'NDVI (Forest Status 2008)');
 
-// Add analytic image as well
-Map.addLayer(analytic, {bands: ['N', 'R', 'G'], min: 0, max: 0.8}, 'Multispectral (NIR/R/G)');
+// Display false color representation.
+var nrgVis = {
+  bands: ['N', 'R', 'G'],
+  min: [156, 62, 53],
+  max: [6408, 2584, 2211],
+  gamma: [0.9, 0.9, 0.9]
+};
+Map.addLayer(analytic, nrgVis, 'Multispectral (NIR/R/G)');
