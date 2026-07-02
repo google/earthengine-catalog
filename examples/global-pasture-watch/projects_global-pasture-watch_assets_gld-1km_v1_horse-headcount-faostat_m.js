d1f106114e02a209456e37e9b4f5ac1416db197f
Leandro Parente <leal.parente@gmail.com>
2026-07-02 14:30:01 +0200

    add gld-1km_v1_headcount-faostat data

--- examples/global-pasture-watch/projects_global-pasture-watch_assets_gld-1km_v1_horse-headcount-faostat_m.js
+++ examples/global-pasture-watch/projects_global-pasture-watch_assets_gld-1km_v1_horse-headcount-faostat_m.js
@@ -1,14 +1,15 @@
 Map.setCenter(-55.50, -12.20, 4);
 
-var horseVis = {
-  min: 0,
-  max: 10,
+var vis = {
+  min: 0, 
+  max: 10, 
   palette: [
-    'ffffcc',
-    'c2e699',
-    '78c679',
-    '31a354',
-    '006837'
+    "d7d7d7",
+    "ffffd4",
+    "fed98e",
+    "fe9929",
+    "d95f0e",
+    "993404"
   ]
 };
 
@@ -16,6 +17,9 @@ var horse = ee.ImageCollection(
   "projects/global-pasture-watch/assets/gld-1km/v1/horse-headcount-faostat_m"
 );
 
-var horse2024 = horse.filterDate('2024-01-01', '2025-01-01').first();
+var horse2000 = horse.filterDate('2000-01-01', '2001-01-01').first();
+var horse2022 = horse.filterDate('2022-01-01', '2023-01-01').first();
 
-Map.addLayer(horse2024, horseVis, 'Cattle headcount (2024)');
+Map.addLayer(horse2000, vis, 'Horse headcount (2000)');
+Map.addLayer(horse2022, vis, 'Horse headcount (2022)');
+
--- examples/global-pasture-watch/projects_global-pasture-watch_assets_gld-1km_v1_sheep-headcount-faostat_m.js
+++ examples/global-pasture-watch/projects_global-pasture-watch_assets_gld-1km_v1_sheep-headcount-faostat_m.js
@@ -1,14 +1,15 @@
-Map.setCenter(-55.50, -12.20, 4);
+Map.setCenter(0, 0, 4);
 
-var sheepVis = {
-  min: 0,
-  max: 160,
+var vis = {
+  min: 0, 
+  max: 160, 
   palette: [
-    'ffffcc',
-    'c2e699',
-    '78c679',
-    '31a354',
-    '006837'
+    "d7d7d7",
+    "ffffd4",
+    "fed98e",
+    "fe9929",
+    "d95f0e",
+    "993404"
   ]
 };
 
@@ -16,6 +17,8 @@ var sheep = ee.ImageCollection(
   "projects/global-pasture-watch/assets/gld-1km/v1/sheep-headcount-faostat_m"
 );
 
-var sheep2024 = sheep.filterDate('2024-01-01', '2025-01-01').first();
+var sheep2000 = sheep.filterDate('2000-01-01', '2001-01-01').first();
+var sheep2022 = sheep.filterDate('2022-01-01', '2023-01-01').first();
 
-Map.addLayer(sheep2024, sheepVis, 'Cattle headcount (2024)');
+Map.addLayer(sheep2000, vis, 'Sheep headcount (2000)');
+Map.addLayer(sheep2022, vis, 'Sheep headcount (2022)');
