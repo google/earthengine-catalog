3758d44d7e9c8c72d683f41e72700e721b763951
Leandro Parente <leal.parente@gmail.com>
2026-07-09 15:19:18 +0200

    simplification of code example for gld-1km

--- examples/global-pasture-watch/projects_global-pasture-watch_assets_gld-1km_v1_livestock-headcount-faostat_m.js
+++ examples/global-pasture-watch/projects_global-pasture-watch_assets_gld-1km_v1_livestock-headcount-faostat_m.js
@@ -8,14 +8,14 @@ var gld = ee.ImageCollection(
   "projects/global-pasture-watch/assets/gld-1km/v1/livestock-headcount-faostat_m"
 );
 
-var cattle2022 = gld.select('cattle').filterDate('2022-01-01', '2023-01-01').first();
-var buffalo2022 = gld.select('buffalo').filterDate('2022-01-01', '2023-01-01').first();
-var horse2022 = gld.select('horse').filterDate('2022-01-01', '2023-01-01').first();
-var goat2022 = gld.select('goat').filterDate('2022-01-01', '2023-01-01').first();
-var sheep2022 = gld.select('sheep').filterDate('2022-01-01', '2023-01-01').first();
+var cattle2022 = gld.filterDate('2022-01-01', '2023-01-01').first();
+var buffalo2022 = gld.filterDate('2022-01-01', '2023-01-01').first();
+var horse2022 = gld.filterDate('2022-01-01', '2023-01-01').first();
+var goat2022 = gld.filterDate('2022-01-01', '2023-01-01').first();
+var sheep2022 = gld.filterDate('2022-01-01', '2023-01-01').first();
 
-Map.addLayer(cattle2022, vis160, 'Cattle headcount (2022)');
-Map.addLayer(buffalo2022, vis160, 'Cattle headcount (2022)');
-Map.addLayer(horse2022, vis10, 'Cattle headcount (2022)');
-Map.addLayer(goat2022, vis160, 'Cattle headcount (2022)');
-Map.addLayer(sheep2022, vis160, 'Buffalo headcount (2022)');
+Map.addLayer(cattle2022.select('cattle'), vis160, 'Cattle headcount (2022)');
+Map.addLayer(buffalo2022.select('buffalo'), vis160, 'Cattle headcount (2022)');
+Map.addLayer(horse2022.select('horse'), vis10, 'Cattle headcount (2022)');
+Map.addLayer(goat2022.select('goat'), vis160, 'Cattle headcount (2022)');
+Map.addLayer(sheep2022.select('sheep'), vis160, 'Buffalo headcount (2022)');
